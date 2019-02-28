341248999e65cbae208e34212ff2edb60e4487ce
KyleTheEdwards <kedwards10@stuy.edu>
2019-02-28 13:10:55 -0500

    Made working copy of the repo Mr. Holmes shared with me

---
+++ UserOfSpeakers.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class UserOfSpeakers {
+  public static void main(String[] args){
+    Animal billsopher = new Retriever();
+    System.out.println(billsopher.speak());
+  }
+}
